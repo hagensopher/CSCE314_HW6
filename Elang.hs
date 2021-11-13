b871a47a19ab86694900d5dd3c5e549254bbcaf7
hagensopher <hagensopher13@gmail.com>
2021-11-13 12:58:53 -0600

    q1 done?

--- Elang.hs
+++ Elang.hs
@@ -9,16 +9,23 @@ data E = IntLit Int
 
 eval :: E -> E
 eval (Equals x y) = BoolLit (eval x == eval y )
---eval (Plus x y) = 
+eval (Plus x y) = plus x y
+eval (Minus x y) = minus x y
+eval (Multiplies x y) = multiply x y
+eval (Exponentiate x y) = exponet x y
 
-plus:: E -> E -> Int
-plus (IntLit x) (IntLit y) = x + y
+plus:: E -> E -> E
+plus (IntLit x) (IntLit y) = IntLit (x + y)
+plus (IntLit x) y = eval y 
 
-multiply:: E -> E -> Int
-multiply (IntLit x) (IntLit y) = x * y
+multiply:: E -> E -> E
+multiply (IntLit x) (IntLit y) = IntLit (x * y)
+multiply (IntLit x) y = eval y 
 
-minus:: E -> E -> Int
-minus (IntLit x) (IntLit y) = x - y
-
-exponet :: E-> E-> Int
-exponet (IntLit x) (IntLit y) = x^y
+minus:: E -> E -> E
+minus (IntLit x) (IntLit y) = IntLit (x - y)
+minus (IntLit x) y = eval y 
+   
+exponet :: E-> E-> E
+exponet (IntLit x) (IntLit y) = IntLit (x^y)
+exponet (IntLit x) y = eval y 
