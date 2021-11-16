3ad5234564c40630f0ce669b442756f3368cafa4
hagensopher <hagensopher13@gmail.com>
2021-11-15 18:34:55 -0600

    added more test cases for q1

--- Elang.hs
+++ Elang.hs
@@ -11,27 +11,34 @@ data E = IntLit Int
 
 eval :: E -> E
 eval (Equals x y) = BoolLit (eval x == eval y )
-eval (Plus x y) = plus x y
-eval (Minus x y) = minus x y
-eval (Multiplies x y) = multiply x y
-eval (Exponentiate x y) = exponet x y
 
-plus:: E -> E -> E
-plus (IntLit x) (IntLit y) = IntLit (x + y)
-plus (IntLit x) y = eval y 
+eval (Plus (IntLit x) (IntLit y)) = IntLit(x+y)
+eval (Plus x y) = eval(Plus (eval(x)) (eval(y)))
 
-multiply:: E -> E -> E
-multiply (IntLit x) (IntLit y) = IntLit (x * y)
-multiply (IntLit x) y = eval y 
+eval (Minus (IntLit x) (IntLit y)) = IntLit (x - y)
+eval (Minus x y) = eval(Minus (eval(x)) (eval(y)))
 
-minus:: E -> E -> E
-minus (IntLit x) (IntLit y) = IntLit (x - y)
-minus (IntLit x) y = eval y 
-   
-exponet :: E-> E-> E
-exponet (IntLit x) (IntLit y) = IntLit (x^y)
-exponet (IntLit x) y = eval y 
+eval (Multiplies (IntLit x) (IntLit y)) = IntLit (x * y)
+eval (Multiplies x y) = eval(Multiplies (eval(x)) (eval(y)))
+
+eval (Exponentiate (IntLit x) (IntLit y)) = IntLit (x^y)
+eval (Exponentiate x y) = eval(Exponentiate (eval(x)) (eval(y)))
+
+eval (BoolLit x) = BoolLit x
+eval (IntLit x) = IntLit x
+
 
 
 log2Sim :: E -> E
+log2Sim (IntLit 1) = IntLit 0
 log2Sim (IntLit x) = IntLit(round (logBase 2 (fromIntegral x)))
+
+log2Sim (Multiplies (IntLit x) (IntLit y)) = Plus (IntLit x) (IntLit y) 
+log2Sim (Multiplies x y) = Plus (log2Sim(x)) (log2Sim(y)) 
+
+log2Sim (Exponentiate (IntLit x)(IntLit y)) = Multiplies (log2Sim(IntLit x)) (IntLit y) -- have to reduce to keep equality
+log2Sim (Exponentiate x y) = Multiplies (log2Sim x) (log2Sim y) -- have to reduce to keep equality
+
+log2Sim (Equals x y) = Equals (log2Sim x) (log2Sim y)
+
+log2Sim (BoolLit x) = (BoolLit x)
