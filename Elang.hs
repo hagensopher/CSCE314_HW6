7c4aa2cf0efd8487b99196c5de936598cf8e0ba0
hagensopher <hagensopher13@gmail.com>
2021-11-17 12:39:13 -0600

    final hw6

--- Elang.hs
+++ Elang.hs
@@ -1,4 +1,5 @@
-
+--Hagen Sopher
+--426004814
 
 data E = IntLit Int
        | BoolLit Bool
@@ -8,7 +9,7 @@ data E = IntLit Int
        | Exponentiate E E
        | Equals E E
          deriving (Eq, Show)
-
+--eval function with all the matching types
 eval :: E -> E
 eval (Equals x y) = BoolLit (eval x == eval y )
 
@@ -27,11 +28,17 @@ eval (Exponentiate x y) = eval(Exponentiate (eval(x)) (eval(y)))
 eval (BoolLit x) = BoolLit x
 eval (IntLit x) = IntLit x
 
-
-
+--helper function to check if a value is logbase 2 (4,8,16,32,...)
+divider:: Int -> Int -> Int-> Int
+divider x y count  
+  | (x `mod` y ) == 0 &&  x>0 = divider (x `div` y ) y (count+1)
+  | x == 1 = count
+  
+-- log conversion with the cases needed
 log2Sim :: E -> E
 log2Sim (IntLit 1) = IntLit 0
-log2Sim (IntLit x) = IntLit(round (logBase 2 (fromIntegral x)))
+--log2Sim (IntLit x) = IntLit(round (logBase 2 (fromIntegral x)))
+log2Sim (IntLit x) = IntLit (divider x 2 0)
 
 log2Sim (Multiplies (IntLit x) (IntLit y)) = Plus (IntLit x) (IntLit y) 
 log2Sim (Multiplies x y) = Plus (log2Sim(x)) (log2Sim(y)) 
